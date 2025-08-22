64b78a85b70f016de2afc6d6e09c35acee7215eb
guiss26 <guissella584@gmail.com>
2025-08-22 12:37:57 -0700

    add navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
+import { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
     return(
         <nav id='top'>
             <Link to="/" className="nav-title">🔮 TAROT MÍSTICO</Link>
-            <ul className="menu">
+
+            {/**Botón hambburguesa */}
+            <button className="hamburguer-button" onClick={toggleMenu}>☰</button>
+
+            <ul className={`menu ${isOpen ? "open" : ""}`}>
                 <li className="menu-item">
-                    <Link to="/" className="nav-link">Inicio</Link>
+                    <Link to="/" className="nav-link" onClick={() => setIsOpen(false)}>Inicio</Link>
                 </li>
                 <li className="menu-item">
-                    <Link to="/cartas-tarot" className="nav-link">Cartas</Link>
+                    <Link to="/cartas-tarot" className="nav-link" onClick={() => setIsOpen(false)}>Cartas</Link>
                 </li>
                 <li className="menu-item">
-                    <Link to="/lectura" className="nav-link">Lectura</Link>
+                    <Link to="/lectura" className="nav-link" onClick={() => setIsOpen(false)}>Lectura</Link>
                 </li>
             </ul>
         </nav>
